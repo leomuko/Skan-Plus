5b24f6a8c75ec68e04eff4236ee5a97c614a6c24
leomuko <kezzieleo@gmail.com>
2021-06-11 15:03:30 +0300

    Implement Barcode Scanning

---
+++ app/src/main/java/com/example/myapplication/BarCodeActivity.kt
@@ -0,0 +1,68 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
+
+class BarCodeActivity : AppCompatActivity() {
+
+    private lateinit var codeScanner: CodeScanner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bar_code)
+
+        codeScanner()
+    }
+
+    private fun codeScanner(){
+        codeScanner = CodeScanner(this, findViewById(R.id.scanner_view))
+
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+            decodeCallback = DecodeCallback {
+                runOnUiThread{
+                    findViewById<TextView>(R.id.tv_textView).text = it.text
+
+                }
+            }
+
+            errorCallback = ErrorCallback {
+                runOnUiThread {
+                    Log.e("Main", "codeScanner: ${it.message}")
+                }
+            }
+        }
+
+        findViewById<CodeScannerView>(R.id.scanner_view).setOnClickListener{
+            codeScanner.startPreview()
+
+        }
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        codeScanner.releaseResources()
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.*
+
+private const val CAMERA_REQUEST_CODE = 101
+
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        setUpPermissions()
+
+        findViewById<Button>(R.id.scan_barcode_btn).setOnClickListener{
+            val intent = Intent(applicationContext, BarCodeActivity::class.java )
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.text_recognition_button).setOnClickListener{
+            Toast.makeText(applicationContext, "To do Text Recognition", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+
+
+    private fun setUpPermissions(){
+        val permission  = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+
+        if(permission != PackageManager.PERMISSION_GRANTED){
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA),
+        CAMERA_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode){
+            CAMERA_REQUEST_CODE-> {
+                if(grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "You need a Camera Permission in order to scan", Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this, "Camera Permission granted", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+}
